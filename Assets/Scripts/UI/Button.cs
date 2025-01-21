a5139230d9e83301492058688c371c7d627e08c2
Munkhbadral Altanpurev <15154972+mbadrala@users.noreply.github.com>
2025-01-21 16:02:00 +0800

    Add button component

---
+++ Assets/Scripts/UI/Button.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class Button : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform body;
+
+    public void OnEnter()
+    {
+        body.DOScale(1.1f, 0.3f);
+    }
+
+    public void OnExit()
+    {
+        body.DOScale(1f, 0.3f);
+    }
+
+    public void OnDown()
+    {
+        body.DOScale(0.9f, 0.1f);
+    }
+
+    public void OnUp()
+    {
+        body.DOScale(1.1f, 0.3f);
+    }
+
+    public void OnClick()
+    {
+
+    }
+}
