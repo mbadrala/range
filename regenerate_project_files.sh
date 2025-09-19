c9b1f2949cea0856bff2542c0cffb5cd5df53d0b
Munkhbadral Altanpurev <15154972+mbadrala@users.noreply.github.com>
2025-09-19 11:32:33 +0800

    Update

--- regenerate_project_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-UNITY_PATH="/Applications/Unity/Hub/Editor/6000.0.32f1/Unity.app/Contents/MacOS/Unity"
-PROJECT_PATH=$(pwd)
-
-echo Regenerating project files for $PROJECT_PATH
-
-"$UNITY_PATH" -projectPath "$PROJECT_PATH" -batchmode -quit -nographics -logFile - -executeMethod "UnityEditor.SyncVS.SyncSolution"
